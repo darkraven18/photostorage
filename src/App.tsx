417643ec4fa467688473e5a553b8992e1a26ccf5
darkraven18 <ravi.ujjwal18@gmail.com>
2025-04-29 00:27:43 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter,  Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './login/login';
 import Home from './home/home';
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 export default App
